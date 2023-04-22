f7f332bbd3c379838902c54572372a33e0854570
ycfan <y_cfan@163.com>
2023-04-22 20:08:36 +0800

    fix bug

--- lua/telescope/_extensions/fly.lua
+++ lua/telescope/_extensions/fly.lua
@@ -1,16 +1,16 @@
-local ok, telescope = pcall(require, 'telescope')
+local ok, telescope = pcall(require, "telescope")
 
 if not ok then
-    error 'Install nvim-telescope/telescope.nvim to use fly-query.nvim.'
+	error("Install nvim-telescope/telescope.nvim to use fly-query.nvim.")
 end
 
-local query = require 'telescope._extensions.fly.picker'
+local query = require("telescope._extensions.fly.picker")
 
-return telescope.register_extension {
-    setup = function(opts, _)
-        -- opts = vim.tbl_extend('force', default_opts, opts)
-    end,
-    exports = {
-        fly_query = query,
-    },
-}
+return telescope.register_extension({
+	setup = function(opts, _)
+		-- opts = vim.tbl_extend('force', default_opts, opts)
+	end,
+	exports = {
+		fly = query,
+	},
+})
